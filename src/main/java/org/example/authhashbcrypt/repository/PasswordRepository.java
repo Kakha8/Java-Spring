d31eab7d279a2530a7866e153a8d1371f7a79447
Kakha8 <Kakha.kudava.98@gmail.com>
2024-10-28 12:07:29 +0200

    Delete src directory

--- src/main/java/org/example/authhashbcrypt/repository/PasswordRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.authhashbcrypt.repository;
-
-public interface PasswordRepository {
-
-    String getSaltFromUser(String username);
-    boolean verPasswd(String password);
-
-    String getPasswordHash(String username);
-}
